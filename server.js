85da43f6ff0575016affcec11ec13e0a54200406
Rakhsanth <35163960+Rakhsanth@users.noreply.github.com>
2020-12-13 18:54:03 +0530

    Bug fixes

--- server.js
+++ server.js
@@ -61,6 +61,8 @@ app.use(
             maxAge: 1000 * 60 * 60 * 24 * process.env.SESSION_EXPIRE, // 1 hour
             httpOnly: true,
             secure: secureCookie,
+            sameSite: none,
+            domain: 'http://localhost:3000/',
         },
         store: store,
         resave: true,
